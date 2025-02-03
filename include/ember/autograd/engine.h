491507be18f86a639ef938e520d8a5f7ec50c525
Kadeem Austin <dev@kadeemaustin.ai>
2025-02-03 10:08:24 -0400

    docs(autograd): add documentation for engine

--- include/ember/autograd/engine.h
+++ include/ember/autograd/engine.h
@@ -1,11 +1,19 @@
-#ifndef ENGINE_H
-#define ENGINE_H
+#ifndef EMBER_AUTOGRAD_ENGINE_H
+#define EMBER_AUTOGRAD_ENGINE_H
 
 #include <ember/autograd/node.h>
 #include <ember/tensor.h>
 #include <unordered_map>
 
 namespace ember::autograd {
+
+/**
+ * @brief The engine that performs backpropagation.
+ *
+ * This class is responsible for performing backpropagation on a computational
+ * graph. It uses a topological sort to evaluate nodes in reverse topological
+ * order.
+ */
 class Engine {
 public:
   Engine() = default;
@@ -16,6 +24,7 @@ private:
   std::unordered_map<Node*, Tensor> grad_buffer;
   void evaluate_fn(Node* func, Tensor gradient);
 };
+
 }  // namespace ember::autograd
 
-#endif  // !ENGINE_H
+#endif  // !EMBER_AUTOGRAD_ENGINE_H
