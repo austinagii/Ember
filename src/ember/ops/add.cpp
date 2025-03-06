652c5baa956eb89304537e5e6d5692b882c16ec4
Kadeem Austin <dev@kadeemaustin.ai>
2025-03-06 16:54:35 -0700

    refactor(ops): redefine addition operation with op registration macro

--- src/ember/ops/add.cpp
+++ src/ember/ops/add.cpp
@@ -12,54 +12,51 @@ namespace ember {
 std::size_t AUGEND_INDEX = 0;
 std::size_t ADDEND_INDEX = 1;
 
-static Tensor add_tensors(Tensor& augend, Tensor& addend) {
-  Tensor sum = Tensor::from_xarray_(xt::eval(augend.data_ + addend.data_));
-
-  if (augend.requires_grad() || addend.requires_grad()) {
-    sum.set_gradient_fn(new AddBackward(augend, addend));
-    sum.get_gradient_fn()->saved_tensors.insert(
-        sum.get_gradient_fn()->saved_tensors.begin(),
-        {augend.save(), addend.save()});
-    sum.requires_grad(true);
-  }
-  return sum;
+static Tensor add_forward(autograd::Context& context, Tensor& augend,
+                          Tensor& addend) {
+  context.save_for_backward(augend);
+  context.save_for_backward(addend);
+  return Tensor::from_xarray_(xt::eval(augend.data_ + addend.data_));
 }
 
-Tensor operator+(Tensor& augend, Tensor& addend) {
-  return add_tensors(augend, addend);
-}
-
-Tensor operator+(Tensor& augend, Tensor&& addend) {
-  return add_tensors(augend, addend);
-}
-
-Tensor operator+(Tensor&& augend, Tensor& addend) {
-  return add_tensors(augend, addend);
-}
-
-Tensor operator+(Tensor&& augend, Tensor&& addend) {
-  return add_tensors(augend, addend);
-}
-
-AddBackward::AddBackward(Tensor& augend, Tensor& addend) {
-  if (augend.requires_grad()) {
-    edges.push_back(autograd::Edge(AUGEND_INDEX, augend.get_gradient_fn()));
-  }
-  if (addend.requires_grad()) {
-    edges.push_back(autograd::Edge(ADDEND_INDEX, addend.get_gradient_fn()));
-  }
-}
-
-std::vector<Tensor> AddBackward::operator()(Tensor output_grad) {
-  auto augend = saved_tensors[AUGEND_INDEX];
-  auto addend = saved_tensors[ADDEND_INDEX];
+/**
+ * @brief Compute the gradients of both inputs of an addition operation (a+b)
+ *
+ * Given that c = a + b and the partial derivate of c w.r.t some output o
+ * (∂c/∂o) is `output_grad`, then the partial derivatives of a and b are as
+ * follows:
+ *
+ * - ∂a/∂o = `output_grad` * 1
+ * - ∂b/∂o = `output_grad` * 1
+ *
+ * @param output_grad The gradient of the output of the addition operation.
+ * @return A vector containing the gradients of the inputs of the addition
+ * operation.
+ */
+std::vector<Tensor> add_backward(autograd::Context& context,
+                                 Tensor output_grad) {
+  auto augend = context.saved_tensors[AUGEND_INDEX];
+  auto addend = context.saved_tensors[ADDEND_INDEX];
 
   auto augend_gradient =
       reduce_broadcast(output_grad.data_, augend.data_.shape());
   auto addend_gradient =
       reduce_broadcast(output_grad.data_, addend.data_.shape());
+
   return {Tensor::from_xarray_(augend_gradient),
           Tensor::from_xarray_(addend_gradient)};
 }
+
+REGISTER_BINARY_OP(add, add_forward, add_backward);
+
+// clang-format off
+Tensor operator+(Tensor& augend, Tensor& addend) { return add(augend, addend); }
+
+Tensor operator+(Tensor& augend, Tensor&& addend) { return add(augend, addend); }
+
+Tensor operator+(Tensor&& augend, Tensor& addend) { return add(augend, addend); }
+
+Tensor operator+(Tensor&& augend, Tensor&& addend) { return add(augend, addend); }
+// clang-format on
 
 }  // namespace ember
